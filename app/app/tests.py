0e872a5040bf1466b288d5df1c58bb784e3ab968
vicky1482001 <vigneshkumar.saravanan@predigle.com>
2024-12-04 19:19:41 +0530

    wait for db command added

--- app/app/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import SimpleTestCase
-
-from app import calc
-
-class Test(SimpleTestCase):
-    def test_subtract(self):
-        res = calc.subtract(2, 3)
-        self.assertEqual(res, 1)
